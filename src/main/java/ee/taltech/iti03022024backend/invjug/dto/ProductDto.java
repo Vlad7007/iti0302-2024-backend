81f37c636a081f117c4f10ca035cd4e6bae6a903
Vladislav Nikiforov <vnikif@ttu.ee>
2024-10-17 02:04:34 +0300

    Dto added

---
+++ src/main/java/ee/taltech/iti03022024backend/invjug/dto/ProductDto.java
@@ -0,0 +1,9 @@
+package ee.taltech.iti03022024backend.invjug.dto;
+
+public record ProductDto() {
+
+    private static String title;
+    private static Long price;
+    private static Long quantity;
+
+}
