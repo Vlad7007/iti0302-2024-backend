849c57eafe49996f0bf22629c31da84088e3ae4b
Vladislav Nikiforov <vnikif@ttu.ee>
2024-11-19 18:17:01 +0200

    Added first admin user credentials to application.properties.
    Implemented the default admin initialization while application run if there are none in the app_user table.
    
    Added message to the user if he doesn't have access to the endpoint.

--- src/main/java/ee/taltech/iti03022024backend/invjug/config/SecurityConfiguration.java
+++ src/main/java/ee/taltech/iti03022024backend/invjug/config/SecurityConfiguration.java
@@ -1,10 +1,21 @@
 package ee.taltech.iti03022024backend.invjug.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ee.taltech.iti03022024backend.invjug.entities.Role;
+import ee.taltech.iti03022024backend.invjug.entities.UserEntity;
+import ee.taltech.iti03022024backend.invjug.errorhandling.ErrorResponse;
 import ee.taltech.iti03022024backend.invjug.jwt.JwtRequestFilter;
+import ee.taltech.iti03022024backend.invjug.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,12 +23,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
 @AllArgsConstructor
 @Configuration
 public class SecurityConfiguration {
 
     private JwtRequestFilter jwtRequestFilter;
+    private final UserRepository userRepository;
     private static final String GOD = "ADMIN";
+    @Autowired
+    private Environment env;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -37,17 +52,37 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/api/products", "/api/categories", "/api/suppliers").hasAnyRole("USER", "MANAGER", GOD)
                         .requestMatchers(HttpMethod.POST, "/api/products", "/api/categories", "/api/suppliers").hasAnyRole("MANAGER", GOD)
                         .requestMatchers("/api/admin/**").hasRole(GOD)
-
-//                        .requestMatchers("/**").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
-//                        .requestMatchers("/api/products").permitAll()
-//                        .requestMatchers("/api/categories").permitAll()
-//                        .requestMatchers("/api/suppliers").permitAll()
                         .anyRequest().authenticated()
+                )
+                .exceptionHandling(handling -> handling
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpStatus.FORBIDDEN.value());
+                            ErrorResponse errorResponse = new ErrorResponse("Permission denied");
+                            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                            response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
+                        })
                 )
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CommandLineRunner createDefaultAdminUser() {
+        return args -> {
+            String adminUsername = env.getProperty("admin.username", "admin");
+            String adminPassword = env.getProperty("admin.password", "12345678");
+            String adminEmail = env.getProperty("admin.email", "admin@example.com");
+            log.info("Trying to create default admin user: {}", adminUsername);
+
+            if (userRepository.findByRole(Role.ROLE_ADMIN).isEmpty()) {
+                UserEntity adminUser = new UserEntity();
+                adminUser.setUsername(adminUsername);
+                adminUser.setPassword(passwordEncoder().encode(adminPassword));
+                adminUser.setEmail(adminEmail);
+                adminUser.setRole(ee.taltech.iti03022024backend.invjug.entities.Role.ROLE_ADMIN);
+                userRepository.save(adminUser);
+            }
+        };
     }
 
 }
--- src/main/java/ee/taltech/iti03022024backend/invjug/repository/UserRepository.java
+++ src/main/java/ee/taltech/iti03022024backend/invjug/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package ee.taltech.iti03022024backend.invjug.repository;
 
+import ee.taltech.iti03022024backend.invjug.entities.Role;
 import ee.taltech.iti03022024backend.invjug.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    List<UserEntity> findByRole(Role role);
+
 }
